91931c85fca7203568a1d04af67c630111034f2c
Kens04 <kento090403@icloud.com>
2025-03-05 23:34:15 +0900

    InputコンポーネントにInputPropsを追加してReact.FCで型定義

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,13 +1,20 @@
-import { InputProps } from "../types/Input";
+interface InputProps {
+  type: string;
+  id: string;
+  name: string;
+  value: string;
+  disabled: boolean;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}
 
-export const Input = ({
+export const Input: React.FC<InputProps> = ({
   type,
   id,
   name,
   value,
   disabled,
   onChange,
-}: InputProps) => {
+}) => {
   return (
     <input
       className="border border-slate-400 w-full rounded-md p-3"
